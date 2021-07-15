633b7e9015815fded9942ea0d6ed564856a90564
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-07-15 09:25:05 -0300

    Add app theme colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    primary: '#265AB3',
+    secontary: '#FFB200',
+
+    subtitle: '#19438B',
+    info: '#0324B3',
+
+    alert_light: '#FD8702',
+    alert_dark: '#B15E00',
+    danger: '#CA1D23',
+
+    text: '#6F6F6F',
+    input_border: '#B6B2B3',
+  },
+};
+
+export default theme;
